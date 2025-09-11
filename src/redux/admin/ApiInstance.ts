4444fb52a46904c486ba7043663879cf01eb4d47
MNoman431 <nomanrehann112233@gmail.com>
2025-09-11 11:37:41 +0500

    again api instance updated

--- src/redux/admin/ApiInstance.ts
+++ src/redux/admin/ApiInstance.ts
@@ -1,10 +1,11 @@
-// src/redux/axiosInstance.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://ecommerce-backend-z158.onrender.com/api || http://localhost:9999/api",
-   // apna backend ka base URL
-  withCredentials: true, // agar cookies/jwt bhejna hai
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:9999/api"
+      : "https://ecommerce-backend-z158.onrender.com/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
